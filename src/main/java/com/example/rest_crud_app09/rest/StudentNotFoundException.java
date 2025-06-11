b04d4e0577413daa6129424d909479e2e9462424
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-11 20:44:58 +0300

    Chore: added new class

---
+++ src/main/java/com/example/rest_crud_app09/rest/StudentNotFoundException.java
@@ -0,0 +1,31 @@
+package com.example.rest_crud_app09.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    @GetMapping("/students/{studentId}")
+    public String getStudent(@PathVariable int studentId) {
+
+        //verificam din nou studentID si dimensiunea listei
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student Id not found- " + studentId);
+
+        }
+        return theStudents.get(studentId);
+
+    }
+}
--- src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
+++ src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
@@ -2,7 +2,9 @@ package com.example.rest_crud_app09.rest;
 
 
 import com.example.rest_crud_app09.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +16,35 @@ import java.util.List;
 
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
 
-        theStudents.add(new Student("Muntean", "Eugen"));
-        theStudents.add(new Student("Ojog", "Maria"));
-        theStudents.add(new Student("Gonzales", "Pedri"));
+        // definim @PostConstructor ca sa incercam studentii nostri o singura data
+        @PostConstruct
+        public void loadData() {
 
-        return theStudents;
-    }
+            theStudents = new ArrayList<>();
+
+            theStudents.add(new Student("Muntean", "Eugen"));
+            theStudents.add(new Student("Ojog", "Maria"));
+            theStudents.add(new Student("Gonzales", "Pedri"));
+
+
+        }
+
+    // definim endpointu-ul pentru students
+        @GetMapping("/students")
+        public List<Student> getStudents() {
+            return theStudents;
+        }
+
+        // definim un endoint ca sa afisam toti studentii nostri dupa index
+
+        @GetMapping("/students/{studentId}")
+        public Student getStudent(@PathVariable int studentId) {
+
+            return theStudents.get(studentId);
+
+        }
 
 }
