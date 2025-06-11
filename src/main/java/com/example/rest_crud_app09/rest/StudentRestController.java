bd005d6fa8a39705b46cae5124d2f902b8f323ff
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-11 20:03:58 +0300

    Chore: Added new package new class student

---
+++ src/main/java/com/example/rest_crud_app09/rest/StudentRestController.java
@@ -0,0 +1,29 @@
+package com.example.rest_crud_app09.rest;
+
+
+import com.example.rest_crud_app09.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
+public class StudentRestController {
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Muntean", "Eugen"));
+        theStudents.add(new Student("Ojog", "Maria"));
+        theStudents.add(new Student("Gonzales", "Pedri"));
+
+        return theStudents;
+    }
+
+}
